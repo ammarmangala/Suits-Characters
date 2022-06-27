dbe200426315f339d99469c8db25e893f58d3d9e
Ammar Mangala Kitungwa <ammar.mangala@gmail.com>
2022-06-27 04:35:19 +0200

    Add the Suits Character Model to the API

---
+++ suits_character_api/suits_character_api/Controllers/SuitsCharacterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace suits_character_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuitsCharacterController : ControllerBase
+    {
+    }
+}
