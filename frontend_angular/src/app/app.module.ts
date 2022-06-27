7e35a6b8424571f93a336190a1bb20347a9d5544
Ammar Mangala Kitungwa <ammar.mangala@gmail.com>
2022-06-28 00:28:14 +0200

    Install Angular Material

--- frontend_angular/src/app/app.module.ts
+++ frontend_angular/src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditCharacterComponent } from './components/edit-character/edit-character.component';
 import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     EditCharacterComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
